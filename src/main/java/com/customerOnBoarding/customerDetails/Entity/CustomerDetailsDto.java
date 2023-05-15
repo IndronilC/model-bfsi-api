9c8c76cfd56fb699846834251dba3cef0e924f19
Chinthu Damodhar Reddy <102827663+chinthudamodharreddy-CH-2022@users.noreply.github.com>
2023-05-16 00:27:25 +0530

    Create CustomerDetailsDto.java

---
+++ src/main/java/com/customerOnBoarding/customerDetails/Entity/CustomerDetailsDto.java
@@ -0,0 +1,31 @@
+package com.customerOnBoarding.customerDetails.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDetailsDto {
+
+    private long customerId;
+
+    private String fullName;
+
+    private String panNumber;
+
+    private String phoneNumber;
+
+    private String aadharNumber;
+
+    private String address;
+
+    /*private String dateOfBirth;*/
+
+    private String email;
+
+    private int active;
+
+
+}
