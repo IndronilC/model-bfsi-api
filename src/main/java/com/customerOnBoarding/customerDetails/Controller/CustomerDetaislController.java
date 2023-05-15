e669d262907fc5c55aec26dc671252740b25c138
Chinthu Damodhar Reddy <102827663+chinthudamodharreddy-CH-2022@users.noreply.github.com>
2023-05-16 00:30:47 +0530

    Update CustomerDetaislController.java

--- src/main/java/com/customerOnBoarding/customerDetails/Controller/CustomerDetaislController.java
+++ src/main/java/com/customerOnBoarding/customerDetails/Controller/CustomerDetaislController.java
@@ -1,6 +1,7 @@
 package com.customerOnBoarding.customerDetails.Controller;
 
 
+import com.customerOnBoarding.customerDetails.Entity.CustomerDetailsDto;
 import com.customerOnBoarding.customerDetails.Entity.Status;
 import com.customerOnBoarding.customerDetails.Entity.CustomerDetails;
 import com.customerOnBoarding.customerDetails.Service.CustomerDetailsService;
@@ -14,40 +15,40 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
-public class CustomerDetaislController {
+public class CustomerDetailsController {
 
     @Autowired
-     CustomerDetailsService customerDetailsService;
+    CustomerDetailsService customerDetailsService;
 
-    @RequestMapping( value = {"/createCustomer"} ,method=RequestMethod.POST)
-    public Status viewCustomer(@RequestBody CustomerDetails customerDetails){
-       return customerDetailsService.customerOnBoarding(customerDetails);
+    @PostMapping(value = {"/createCustomer"} /*,method=RequestMethod.POST*/)
+    public Status viewCustomer(@RequestBody CustomerDetails customerDetailsDto) {
+        return customerDetailsService.customerOnBoarding(customerDetailsDto);
 
 
     }
 
 
-    @RequestMapping(value = {"/id"},method = RequestMethod.GET)
-    public Optional<CustomerDetails> getById(@RequestBody CustomerDetails customerDetails){
+    @GetMapping(value = {"/id"}/*,method = RequestMethod.GET*/)
+    public Optional<CustomerDetails> getById(@RequestBody CustomerDetails customerDetails) {
         return customerDetailsService.getCustomerByCustomerId(customerDetails.getCustomerId());
 
     }
 
-    @RequestMapping(value = {"/allCustomerDetails"},method = RequestMethod.GET)
-    public List<CustomerDetails> getAllCustomerDetails(){
+    @GetMapping(value = {"/allCustomerDetails"}/*,method = RequestMethod.GET*/)
+    public List<CustomerDetails> getAllCustomerDetails() {
         return customerDetailsService.getAllCustomerDetails();
     }
 
-    @RequestMapping(value = {"/deleteCustomerBy/{id}"}, method=RequestMethod.DELETE)
-    public Status deleteCustomerById(@PathVariable("id") Long customerId){
+    @DeleteMapping(value = {"/deleteCustomerBy/{id}"}/*, method=RequestMethod.DELETE*/)
+    public Status deleteCustomerById(@PathVariable("id") Long customerId) {
 
         return customerDetailsService.deleteCustomerById(customerId);
     }
 
     @RequestMapping(value = {"/customerDetails/{id}"})
-    public CustomerDetails updateCustomerDetailsById( @RequestBody  CustomerDetails customerDetails,
-                                                      @PathVariable("id") Long customerId){
-        return customerDetailsService.updateCustomerDetails(customerDetails,customerId);//.getBody();
+    public CustomerDetails updateCustomerDetailsById(@RequestBody CustomerDetails customerDetails,
+                                                     @PathVariable("id") Long customerId) {
+        return customerDetailsService.updateCustomerDetails(customerDetails, customerId);//.getBody();
     }
 
 
